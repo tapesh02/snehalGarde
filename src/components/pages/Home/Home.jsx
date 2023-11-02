c85555d53fb477978969e23992d181b8e1a32603
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2023-11-02 00:20:35 +0000

    Added home page content

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import "./home.scss";
+import { Typography } from "@mui/material";
 
 const Home = () => {
     return (
@@ -8,7 +9,12 @@ const Home = () => {
             <div className="homeBg">
                 <div className="homeBg2"></div>
             </div>
-            <div className="">Home</div>
+            <div className="header">
+                <Typography variant="h6">Committed to Helping Our Clients Succeed</Typography>
+                <Typography variant="h3">
+                    You Need a Reliable Law Firm That Helps Deal with Your Family Matters
+                </Typography>
+            </div>
         </>
     );
 };
