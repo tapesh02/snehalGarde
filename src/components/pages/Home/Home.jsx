821a0067153cc05ec2dd763c4cc02ba63e9fb95c
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2023-11-04 03:17:18 +0000

    Added responsive header styles

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 import { Typography } from "@mui/material";
 
 const Home = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
     return (
         <>
             <div className="homeBg">
                 <div className="homeBg2"></div>
             </div>
-            <div className="header">
-                <Typography variant="h6">Committed to Helping Our Clients Succeed</Typography>
-                <Typography variant="h3">
+            <div className={`header ${isMobile && "mobileheader"}`}>
+                <Typography variant="h6" className={`${isMobile && "mobileh6"}`}>
+                    Committed to Helping Our Clients Succeed
+                </Typography>
+                <Typography variant="h3" className={`${isMobile && "mobileh3"}`}>
                     You Need a Reliable Law Firm That Helps Deal with Your Family Matters
                 </Typography>
             </div>
