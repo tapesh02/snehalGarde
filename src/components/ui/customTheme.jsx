bc9e38ff93c2d3dc454e820ba60e666c880c3967
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-06-29 09:48:46 +0100

    fixed the bug

--- src/components/ui/customTheme.jsx
+++ src/components/ui/customTheme.jsx
@@ -1,4 +1,4 @@
-import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 export const customTheme = () => {
   return createTheme({
@@ -27,4 +27,4 @@ export const customTheme = () => {
   });
 };
 
-export default responsiveFontSizes(customTheme);
+export default customTheme;
