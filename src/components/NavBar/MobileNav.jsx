68f75c8f2b63c67e786e86933e01484dd3f14f02
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-07-20 17:04:00 +0100

    Added navigation

--- src/components/NavBar/MobileNav.jsx
+++ src/components/NavBar/MobileNav.jsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import * as React from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -14,80 +15,80 @@ import { AiFillHome, AiFillContacts, AiOutlineTeam } from "react-icons/ai";
 import { BsFillAwardFill, BsMenuButtonWideFill } from "react-icons/bs";
 
 export default function MobileNav() {
-    const [state, setState] = React.useState({
-        top: false,
-    });
+  const [state, setState] = React.useState({
+    top: false,
+  });
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
-            return;
-        }
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+      return;
+    }
 
-        setState({ ...state, [anchor]: open });
-    };
+    setState({ ...state, [anchor]: open });
+  };
 
-    const list = (anchor) => (
-        <Box
-            sx={{ width: anchor === "top" ? "auto" : 250 }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}>
-            <List>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AiFillHome />
-                        </ListItemIcon>
-                        <ListItemText primary="Home" />
-                    </ListItemButton>
-                </ListItem>
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/">
+            <ListItemIcon>
+              <AiFillHome />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
 
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AiOutlineTeam />
-                        </ListItemIcon>
-                        <ListItemText primary="About" />
-                    </ListItemButton>
-                </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/about">
+            <ListItemIcon>
+              <AiOutlineTeam />
+            </ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItemButton>
+        </ListItem>
 
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AiFillContacts />
-                        </ListItemIcon>
-                        <ListItemText primary="Contact" />
-                    </ListItemButton>
-                </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/contact">
+            <ListItemIcon>
+              <AiFillContacts />
+            </ListItemIcon>
+            <ListItemText primary="Contact" />
+          </ListItemButton>
+        </ListItem>
 
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <BsFillAwardFill />
-                        </ListItemIcon>
-                        <ListItemText primary="Practice" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-            <Divider />
-            <div style={{ width: "100%" }}>
-                <Button variant="contained" style={{ width: "100%" }}>
-                    Free Consultation
-                </Button>
-            </div>
-        </Box>
-    );
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/practice">
+            <ListItemIcon>
+              <BsFillAwardFill />
+            </ListItemIcon>
+            <ListItemText primary="Practice" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <div style={{ width: "100%" }}>
+        <Button variant="contained" style={{ width: "100%" }} component={Link} to="/freeConsultation">
+          Free Consultation
+        </Button>
+      </div>
+    </Box>
+  );
 
-    return (
-        <div>
-            {["top"].map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <BsMenuButtonWideFill onClick={toggleDrawer(anchor, true)}>{anchor}</BsMenuButtonWideFill>
-                    <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-                        {list(anchor)}
-                    </Drawer>
-                </React.Fragment>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {["top"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <BsMenuButtonWideFill onClick={toggleDrawer(anchor, true)}>{anchor}</BsMenuButtonWideFill>
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
